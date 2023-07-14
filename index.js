160cd6f33e2c78b837c01fe6c8003635b05f8b43
Sam George Mathew <37585951+SGM-dev@users.noreply.github.com>
2023-07-14 15:02:55 +0530

    render multiple dice

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const hero = {
   name: "Wizard",
   avatar: "images/wizard.png",
   health: 60,
-  diceRoll: 6,
+  diceRoll: 3,
+  diceCount: 3,
 };
 
 const monster = {
@@ -12,20 +13,25 @@ const monster = {
   avatar: "images/orc.png",
   health: 10,
   diceRoll: 4,
+  diceCount: 1,
 };
 
 function renderCharacter(data) {
-  const { elementId, name, avatar, health, diceRoll } = data;
+  const { elementId, name, avatar, health, diceRoll, diceCount } = data;
 
-  document.getElementById(elementId).innerHTML = `
-<div class="character-card">
-   <h4 class="name"> ${name} </h4>
-    <img class="avatar" src="${avatar}"/>
-    <p class="health">health: <b> ${health} </b></p>
-    <div class="dice-container">
-      <div class="dice"> ${diceRoll} </div>
-    </div>
-</div>`;
+  let diceHtml = ``;
+  for (let i = 0; i < diceCount; i++) {
+    diceHtml += `<div class="dice">${diceRoll}</div>`;
+  }
+
+  document.getElementById(elementId).innerHTML = `<div class="character-card">
+           <h4 class="name"> ${name} </h4>
+           <img class="avatar" src="${avatar}" />
+           <div class="health">health: <b> ${health} </b></div>
+           <div class="dice-container">
+               ${diceHtml}
+           </div>
+       </div>`;
 }
 
 renderCharacter(hero);
