ddd751b42d88ecdea79c1e79ec182a45243db390
Sam George Mathew <37585951+SGM-dev@users.noreply.github.com>
2023-07-15 22:23:34 +0530

    use the Array constructor in getDiceRollArray()

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 function getDiceRollArray(diceCount) {
-  let newDiceRolls = [];
-  for (let i = 0; i < diceCount; i++) {
-    newDiceRolls.push(Math.floor(Math.random() * 6) + 1);
-  }
-  return newDiceRolls;
+  return new Array(diceCount)
+    .fill(0)
+    .map(() => Math.floor(Math.random() * 6) + 1);
 }
 
 function getDiceHtml(diceCount) {
   return getDiceRollArray(diceCount)
-    .map((num) => {
+    .map(() => {
       return `<div class="dice">${num}</div>`;
     })
     .join("");
