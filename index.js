9c1ac7a77bae6c25b5dfef014354899b41ef120c
Sam George Mathew <37585951+SGM-dev@users.noreply.github.com>
2023-07-15 22:05:06 +0530

    render dice with random numbers

--- index.js
+++ index.js
@@ -6,14 +6,19 @@ function getDiceRollArray(diceCount) {
   return newDiceRolls;
 }
 
-console.log(getDiceRollArray(3));
+function getDiceHtml(diceCount) {
+  return getDiceRollArray(diceCount)
+    .map((num) => {
+      return `<div class="dice">${num}</div>`;
+    })
+    .join("");
+}
 
 const hero = {
   elementId: "hero",
   name: "Wizard",
   avatar: "images/wizard.png",
   health: 60,
-  diceRoll: [3, 1, 4],
   diceCount: 3,
 };
 
@@ -22,18 +27,13 @@ const monster = {
   name: "Orc",
   avatar: "images/orc.png",
   health: 10,
-  diceRoll: [2],
   diceCount: 1,
 };
 
 function renderCharacter(data) {
-  const { elementId, name, avatar, health, diceRoll, diceCount } = data;
+  const { elementId, name, avatar, health, diceCount } = data;
 
-  const diceHtml = diceRoll
-    .map((num) => {
-      return `<div class="dice">${num}</div>`;
-    })
-    .join("");
+  const diceHtml = getDiceHtml(diceCount);
 
   document.getElementById(elementId).innerHTML = `<div class="character-card">
            <h4 class="name"> ${name} </h4>
