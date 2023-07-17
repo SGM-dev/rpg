ba9f51ce9a5bdb4e0110ddc32f16276637792ad1
Sam George Mathew <37585951+SGM-dev@users.noreply.github.com>
2023-07-17 22:57:00 +0530

    use method on the constructor function to render

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 function getDiceRollArray(diceCount) {
-  return new Array(diceCount)
-    .fill(0)
-    .map(() => Math.floor(Math.random() * 6) + 1);
+  return new Array(diceCount).fill(0).map(function () {
+    return Math.floor(Math.random() * 6) + 1;
+  });
 }
 
 function getDiceHtml(diceCount) {
   return getDiceRollArray(diceCount)
-    .map(() => {
-      return `<div class="dice">${diceCount}</div>`;
+    .map(function (num) {
+      return `<div class="dice">${num}</div>`;
     })
     .join("");
 }
@@ -34,22 +34,23 @@ function Character(data) {
   this.avatar = data.avatar;
   this.health = data.health;
   this.diceCount = data.diceCount;
+  this.getCharacterHtml = function () {
+    const { elementId, name, avatar, health, diceCount } = this;
+    const diceHtml = getDiceHtml(diceCount);
+
+    document.getElementById(elementId).innerHTML = `<div class="character-card">
+          <h4 class="name"> ${name} </h4>
+          <img class="avatar" src="${avatar}" />
+          <div class="health">health: <b> ${health} </b></div>
+          <div class="dice-container">    
+              ${diceHtml}
+          </div>
+      </div>`;
+  };
 }
 
-function renderCharacter(data) {
-  const { elementId, name, avatar, health, diceCount } = data;
+const wizard = new Character(hero);
+wizard.getCharacterHtml();
 
-  const diceHtml = getDiceHtml(diceCount);
-
-  document.getElementById(elementId).innerHTML = `<div class="character-card">
-           <h4 class="name"> ${name} </h4>
-           <img class="avatar" src="${avatar}" />
-           <div class="health">health: <b> ${health} </b></div>
-           <div class="dice-container">
-               ${diceHtml}
-           </div>
-       </div>`;
-}
-
-renderCharacter(hero);
-renderCharacter(monster);
+const orc = new Character(monster);
+orc.getCharacterHtml();
