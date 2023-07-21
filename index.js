efd2aa7a065ce4512c085333b522e0d2fa82e7a2
Sam George Mathew <37585951+SGM-dev@users.noreply.github.com>
2023-07-21 08:52:15 +0530

    Wire up the attack button

--- index.js
+++ index.js
@@ -2,13 +2,18 @@ import characterData from "./data.js";
 
 import Character from "./Character.js";
 
-const wizard = new Character(characterData.hero);
-const orc = new Character(characterData.monster);
-
 function render() {
   document.getElementById("hero").innerHTML = wizard.getCharacterHtml();
 
   document.getElementById("monster").innerHTML = orc.getCharacterHtml();
 }
 
+function attack() {
+  console.log("attack button working!");
+}
+
+const wizard = new Character(characterData.hero);
+const orc = new Character(characterData.monster);
 render();
+
+document.getElementById("attack-button").addEventListener("click", attack);
