7169d814f0bab54a03e1f865d2e1039960cb83d1
Sam George Mathew <37585951+SGM-dev@users.noreply.github.com>
2023-07-22 22:10:48 +0530

    finish up endGame()

--- index.js
+++ index.js
@@ -20,7 +20,21 @@ function attack() {
 }
 
 function endGame() {
-  console.log("the game is over")
+  const endMessage =
+    wizard.health === 0 && orc.health === 0
+      ? "No victors - all creatures are dead"
+      : wizard.health > 0
+      ? "The Wizard Wins"
+      : "The Orc is Victorious";
+
+  const endEmoji = wizard.health > 0 ? "🔮" : "☠️";
+
+  document.querySelector("body").innerHTML = `
+  <div class="end-game">
+    <h2>Game Over</h2>
+    <h3>${endMessage}</h3>
+    <p class="end-emoji">${endEmoji}</p>
+  </div>`;
 }
 
 const wizard = new Character(characterData.hero);
