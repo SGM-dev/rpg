0b7becb66b0f9e60df0079fbd3026a3ab6c8a89c
Sam George Mathew <37585951+SGM-dev@users.noreply.github.com>
2023-07-24 15:13:22 +0530

    display health bar

--- Character.js
+++ Character.js
@@ -13,6 +13,16 @@ function Character(data) {
       .join("");
   };
 
+  this.getHealthBarHtml = function () {
+    const percent = getPercentage(this.health, this.maxHealth);
+    return `
+    <div class="health-bar-outer">
+      <div class="health-bar-inner ${percent <= 25 ? "danger" : ""} " 
+        style="width: ${percent}%;">
+      </div>
+    </div>`;
+  };
+
   this.takeDamage = function (currentDiceScore) {
     const totalAttackScore = currentDiceScore.reduce(
       (total, num) => total + num
@@ -26,11 +36,13 @@ function Character(data) {
 
   this.getCharacterHtml = function () {
     const { elementId, name, avatar, health, diceArray } = this;
+    const healthBar = this.getHealthBarHtml();
 
     return `<div class="character-card">
             <h4 class="name"> ${name} </h4>
             <img class="avatar" src="${avatar}" />
             <div class="health">health: <b> ${health} </b></div>
+            ${healthBar}
             <div class="dice-container">    
                 ${diceArray}
             </div>
